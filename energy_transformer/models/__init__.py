89be302e1b73e6afd25c6d8dccf905d9b602ee8e
Ayan Das <bvits@riseup.net>
2025-05-24 03:45:13 +0530

    chore: Remove deprecated vision components from model exports
    - Drop ViETEncoder, MLPDecoder, ClassificationHead, LogitsHead
    - Keep only EnergyTransformer and REALISER_REGISTRY in __all__

--- energy_transformer/models/__init__.py
+++ energy_transformer/models/__init__.py
@@ -1,20 +1,8 @@
 """Energy Transformer models."""
 
 from .base import REALISER_REGISTRY, EnergyTransformer
-from .vision import (
-    ClassificationHead,
-    LogitsHead,
-    MLPDecoder,
-    ViETEncoder,
-    assemble_encoder,
-)
 
 __all__ = [
     "EnergyTransformer",
-    "ViETEncoder",
-    "ClassificationHead",
-    "MLPDecoder",
-    "LogitsHead",
-    "assemble_encoder",
-    "REALISER_REGISTRY",
+    "REALISER_REGISTRY"
 ]
