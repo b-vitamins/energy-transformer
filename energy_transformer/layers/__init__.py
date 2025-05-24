fec6b0564ddb2d0af78c52cb19bcffc30abfdaed
Ayan Das <bvits@riseup.net>
2025-05-24 13:28:19 +0530

    feat: Update layer exports to include simplicial and embedding APIs
    - Add exports for `PatchEmbedding`, `PositionalEmbedding2D`
    - Add `ClassificationHead` and `FeatureHead` to module interface
    - Replace `SimplicialHopfieldNetwork` with detailed simplex-related classes

--- energy_transformer/layers/__init__.py
+++ energy_transformer/layers/__init__.py
@@ -1,11 +1,29 @@
 """Energy Transformer layers."""
 
 from .attention import MultiHeadEnergyAttention
+from .embeddings import PatchEmbedding, PositionalEmbedding2D
+from .heads import ClassificationHead, FeatureHead
 from .hopfield import HopfieldNetwork
 from .layer_norm import LayerNorm
+from .simplicial import (
+    SimplexGenerator,
+    SimplexQuery,
+    SimplicialComplex,
+    unrank,
+)
+from .tokens import CLSToken
 
 __all__ = [
-    "MultiHeadEnergyAttention",
-    "LayerNorm",
+    "ClassificationHead",
+    "CLSToken",
+    "FeatureHead",
     "HopfieldNetwork",
+    "LayerNorm",
+    "MultiHeadEnergyAttention",
+    "PatchEmbedding",
+    "PositionalEmbedding2D",
+    "SimplicialComplex",
+    "SimplexGenerator",
+    "SimplexQuery",
+    "unrank",
 ]
