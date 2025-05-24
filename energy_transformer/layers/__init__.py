62fcfdc7bd7037665d258bb4c83f4763666a016e
Ayan <147558648+b-vitamins@users.noreply.github.com>
2025-05-24 05:33:22 +0000

    Remove deprecated simplicial network import

--- energy_transformer/layers/__init__.py
+++ energy_transformer/layers/__init__.py
@@ -3,11 +3,9 @@
 from .attention import MultiHeadEnergyAttention
 from .hopfield import HopfieldNetwork
 from .layer_norm import LayerNorm
-from .simplicial import SimplicialHopfieldNetwork
 
 __all__ = [
     "MultiHeadEnergyAttention",
     "LayerNorm",
     "HopfieldNetwork",
-    "SimplicialHopfieldNetwork",
 ]
